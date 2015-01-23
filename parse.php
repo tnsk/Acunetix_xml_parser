eab3fab78c5d7c3f3d9c82f1f0d73ca234075f78
yahya <tnsk@users.noreply.github.com>
2015-01-23 02:18:15 +0200

    Update parse.php

--- parse.php
+++ parse.php
@@ -3,22 +3,27 @@ set_time_limit(0);
 /*
 * yhytnsk 
 * Date : 14-10-2015
-* sadece kritik seviyedeki açıkları ekrana dökmektedir. ilgili kısmı düzeltmek isteyenler 
 */
-$xml = simplexml_load_file("acunetin_report.xml");
+$config["kritik"]	=true;//red
+$config["orta"]		=true;//orange
+$config["dusuk"]	=true;//blue
+$config["bilgi"]	=true;//green
 
+$xml = simplexml_load_file("acunetin_report.xml");
 
 $taramalar = $xml->Scan->ReportItems->ReportItem;
 
 function temizle($gelen)
 {
 	$temiz = str_replace("<br />","",$gelen);
+	
 	return $temiz;
 }
 
 function MetotGetir($gelen)
 {
 	$getmi = strpos($gelen,"GET");
+	
 	if($getmi)
 		{
 			return "GET";
@@ -32,19 +37,61 @@ function MetotGetir($gelen)
 $say = 1;
 foreach($taramalar as $x)
 {
-	if($x["color"] == "red")
+	if($config["kritik"]==true)
 	{
-		//echo $x->Name."<br>";
-		echo "Bulgu ".$say.") ".$x->Name." \n";
-		echo "URL : ".$xml->Scan->StartURL.$x->Affects."\n";
-		echo "Parametre : ".$x->Parameter."\n";
-		echo "Method : ".MetotGetir($x->TechnicalDetails->Request)."\n";
-		echo "Request : \n".temizle(nl2br($x->TechnicalDetails->Request))."\n";
-		
-		
-		
-		echo "\n\n";
-		$say = $say+1;
+		if($x["color"] == "red")
+		{
+			echo "Bulgu ".$say.") ".$x->Name." \n";
+			echo "URL : ".$xml->Scan->StartURL.$x->Affects."\n";
+			echo "Parametre : ".$x->Parameter."\n";
+			echo "Method : ".MetotGetir($x->TechnicalDetails->Request)."\n";
+			echo "Request : \n".temizle(nl2br($x->TechnicalDetails->Request))."\n";
+			echo "\n\n";
+			
+			$say = $say+1;
+		}
+	}
+	if($config["orta"]==true)
+	{
+		if($x["color"] == "orange")
+		{
+			echo "Bulgu ".$say.") ".$x->Name." \n";
+			echo "URL : ".$xml->Scan->StartURL.$x->Affects."\n";
+			echo "Parametre : ".$x->Parameter."\n";
+			echo "Method : ".MetotGetir($x->TechnicalDetails->Request)."\n";
+			echo "Request : \n".temizle(nl2br($x->TechnicalDetails->Request))."\n";
+			echo "\n\n";
+			
+			$say = $say+1;
+		}
+	}
+	if($config["dusuk"]==true)
+	{
+		if($x["color"] == "blue")
+		{
+			echo "Bulgu ".$say.") ".$x->Name." \n";
+			echo "URL : ".$xml->Scan->StartURL.$x->Affects."\n";
+			echo "Parametre : ".$x->Parameter."\n";
+			echo "Method : ".MetotGetir($x->TechnicalDetails->Request)."\n";
+			echo "Request : \n".temizle(nl2br($x->TechnicalDetails->Request))."\n";
+			echo "\n\n";
+			
+			$say = $say+1;
+		}
+	}
+	if($config["bilgi"]==true)
+	{
+		if($x["color"] == "green")
+		{
+			echo "Bulgu ".$say.") ".$x->Name." \n";
+			echo "URL : ".$xml->Scan->StartURL.$x->Affects."\n";
+			echo "Parametre : ".$x->Parameter."\n";
+			echo "Method : ".MetotGetir($x->TechnicalDetails->Request)."\n";
+			echo "Request : \n".temizle(nl2br($x->TechnicalDetails->Request))."\n";
+			echo "\n\n";
+			
+			$say = $say+1;
+		}
 	}
 	
 }
